29dcd71eedb37faffaf133618d984d950b012fdf
596326928 <596326928@qq.com>
2016-06-23 21:15:58 +0800

    zhuChat

---
+++ 456464684645/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  456464684645
+//
+//  Created by student on 16/6/23.
+//  Copyright © 2016年 ZhangJie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 456464684645/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  456464684645
+//
+//  Created by student on 16/6/23.
+//  Copyright © 2016年 ZhangJie. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
